35fd98025c6ce9c60fb50620ec7c26bd485318b7
LudaKend <Ludakend@gmail.com>
2024-03-14 16:57:59 +0300

    эндпоинты для привычек; сериализатор, пермишен и пагинатор к ним;

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from rest_framework import generics
+from habits.models import Habit
+from habits.serializers import HabitSerializer
+from rest_framework.permissions import IsAuthenticated
+from habits.permissions import AuthorPermissionsClass
+from habits.paginators import HabitsPaginator
 
-# Create your views here.
+
+
+class HabitCreateAPIView(generics.CreateAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        """метод для записи авторизованного пользователя в качестве автора """
+        #print(self.request.user)   #для отладки
+        habit = serializer.save(author=self.request.user)
+        habit.save()
+
+
+class HabitListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated & AuthorPermissionsClass]
+    pagination_class = HabitsPaginator
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        owner_queryset = queryset.filter(author=self.request.user)
+        return owner_queryset
+
+class HabitPublicListAPIView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated]
+    pagination_class = HabitsPaginator
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        owner_queryset = queryset.filter(is_public=True)
+        return owner_queryset
+
+class HabitUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated & AuthorPermissionsClass]
+
+
+class HabitDestroyAPIView(generics.DestroyAPIView):
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated & AuthorPermissionsClass]
+
