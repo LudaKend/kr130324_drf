a48de48b813674019aa1c19a8bed75dc5957de18
LudaKend <Ludakend@gmail.com>
2024-03-25 09:09:04 +0300

    доработка по замечаниям наставника: сделала обязательными к заполнению поля: место, время, привычка(действие), автор(создатель привычки), дата создания, время выполнения, периодичность.

--- habits/models.py
+++ habits/models.py
@@ -6,20 +6,19 @@ NULLABLE = {'null': True, 'blank': True}
 
 
 class Habit(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, verbose_name='Автор',
-                               **NULLABLE)
-    place = models.CharField(max_length=100, verbose_name='Место', **NULLABLE)
-    habit_time = models.TimeField(verbose_name='Время', **NULLABLE)
-    habit = models.CharField(max_length=500, verbose_name='Привычка', **NULLABLE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, verbose_name='Автор')
+    place = models.CharField(max_length=100, verbose_name='Место')
+    habit_time = models.TimeField(verbose_name='Время', default='09:00:00')
+    habit = models.CharField(max_length=500, verbose_name='Привычка')
     is_nice = models.BooleanField(verbose_name='Приятная привычка', default=False)
     attached_habit = models.IntegerField(verbose_name='Привязанная привычка', **NULLABLE)
-    period = models.IntegerField(verbose_name='Периодичность в днях', validators=[MaxValueValidator(7)], **NULLABLE)
+    period = models.IntegerField(verbose_name='Периодичность в днях', validators=[MaxValueValidator(7)], default='1')
     bonus = models.CharField(max_length=500, verbose_name='Вознаграждение', **NULLABLE)
 
     lead_time_new = models.IntegerField(verbose_name='Время на  выполнение привычки в секундах',
-                                        validators=[MaxValueValidator(120)], **NULLABLE)  # не больше 120секунд!
+                                        validators=[MaxValueValidator(120)], default='1')  # не больше 120секунд!
     is_publish = models.BooleanField(verbose_name='Общий доступ', default=False)
-    data_create = models.DateField(verbose_name='дата создания', auto_now_add=True, **NULLABLE)
+    data_create = models.DateField(verbose_name='дата создания', auto_now_add=True)
 
     def __str__(self):
         '''строковое отображение обьекта'''
